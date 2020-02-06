2b077aafbb32b5a86ea35cb85e0497f69f5f1b38
divakarr2017 <divakarr@simsaw.com>
2020-02-06 15:19:48 +0530

    Updates

---
+++ precache-manifest.9daffbab0eaf708ca011fc05c02d3f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bfcf07d5ab8c951022948be300b1b4",
+    "url": "/react2020/index.html"
+  },
+  {
+    "revision": "7315d4de4ad348627f4a",
+    "url": "/react2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b7d1f5da8f68d9c85f7",
+    "url": "/react2020/static/js/2.c044d5dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react2020/static/js/2.c044d5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7315d4de4ad348627f4a",
+    "url": "/react2020/static/js/main.73282fab.chunk.js"
+  },
+  {
+    "revision": "216004dc4edbf372ac00",
+    "url": "/react2020/static/js/runtime-main.b5ec1262.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
