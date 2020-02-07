75b658ef5bafcfbf17a67c03a34135133ad87664
divakarr2017 <divakarr@simsaw.com>
2020-02-07 10:48:43 +0530

    Layout changes

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,20 +1,102 @@
 import React from 'react';
- 
-const contactus = () => {
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
     return (
-       <div>
-          <h1>Contact us</h1>
-           <p>Learn React 2020
-        <p><b>Divakar Rattan</b>
-            @divakartec09
-            Do more than belong: participate. 
-            Do more than care: help. 
-            Do more than believe: practice. 
-            Do more than be fair: be kind. 
-            Do more than forgive: forget. Do more
-            </p></p>
-       </div>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://divakarr2017.github.io/react2020/">
+         react2020
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
     );
-}
- 
-export default contactus;
+  }
+  
+  const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  
+  export default function ContactUs() {
+    const classes = useStyles();
+  
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Contact us
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+             <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="mobile"
+              label="Mobile Number"
+              name="mobile"
+              autoComplete="mobile"
+              autoFocus
+            />
+         
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              contact us
+            </Button>
+           
+          </form>
+        </div>
+        
+      </Container>
+    );
+  }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,103 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { NavLink } from 'react-router-dom';
- 
-const home = () => {
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import SignIn from './SignIn';
+import Error from './Error';
+import Navigation from './Navigation';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Copyright() {
     return (
-       <div>
-          <h1>Home</h1>
-          <BrowserRouter>
-                <div>
-                    <NavLink to="/" style={{texDecoration: 'none', color: 'black', margin:'10px'}}>Home</NavLink>
-                    <NavLink to="/about" style={{texDecoration: 'none', color: 'black', margin:'10px'}}>About Us</NavLink>
-                    <NavLink to="/contact" style={{texDecoration: 'none', color: 'black', margin:'10px'}}>Contact Us</NavLink>
-                </div>
-       </BrowserRouter>
-       Learn React 2020
-        <p><b>Divakar Rattan</b>
-        @divakartec09
-        Do more than belong: participate. 
-        Do more than care: help. 
-        Do more than believe: practice. 
-        Do more than be fair: be kind. 
-        Do more than forgive: forget. Do more
-        </p>
-       </div>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://divakarr2017.github.io/react2020/">
+         react2020
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
     );
-}
- 
-export default home;
+  }
+  
+  const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  
+  export default function Home() {
+    const classes = useStyles();
+  
+    return (
+      <Container component="main">
+        <CssBaseline />
+           welcome 2020 dashboard
+        <div className={classes.paper}>
+      
+        <AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>    
+       
+            <Typography className={classes.root}>
+            <Button color="inherit"><Link href="/react2020/about"  color="inherit" >
+                 AboutUs
+            </Link></Button>
+            <Button color="inherit"><Link href="/react2020/contact" color="inherit">
+            ContactUs
+            </Link></Button>
+            <Button color="inherit"><Link href="/react2020/signIn"   color="inherit">SignIn</Link></Button>
+            <Button color="inherit"><Link href="/react2020/"  color="inherit" >
+                 back to Home
+            </Link></Button>
+            </Typography>
+          
+        </Toolbar>
+        </AppBar>
+            
+        <BrowserRouter>      
+          <Navigation />
+            <Switch>
+            
+             <Route path="/react2020/about" component={AboutUs}/>
+             <Route path="/react2020/contact" component={ContactUs}/>
+             <Route path="/react2020/signIn" component={SignIn}/>
+            
+           </Switch>
+         </BrowserRouter>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
