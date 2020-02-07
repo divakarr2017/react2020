079235cb78b9919c82ae852c7988272119477028
divakarr2017 <divakarr@simsaw.com>
2020-02-07 17:22:58 +0530

    check

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
@@ -12,6 +11,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import ContactIntegration from './ContactIntegration';
+import { Label, TextArea, Input, TextField } from '@material-ui/core';
 
 function Copyright() {
     return (
@@ -59,42 +60,10 @@ function Copyright() {
           <Typography component="h1" variant="h5">
             Contact us
           </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-             <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="mobile"
-              label="Mobile Number"
-              name="mobile"
-              autoComplete="mobile"
-              autoFocus
-            />
-         
+          <div>
+         <ContactIntegration />
             
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              contact us
-            </Button>
-           
-          </form>
+            </div>
         </div>
         
       </Container>
